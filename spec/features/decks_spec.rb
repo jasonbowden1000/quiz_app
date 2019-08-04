9c6ed09dbfd53ce5ef18382a2b04a00b73482d17
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-04 15:42:51 -0500

    Displaying Decks

--- spec/features/decks_spec.rb
+++ spec/features/decks_spec.rb
@@ -2,26 +2,45 @@ require "rails_helper"
 
 RSpec.describe 'Decks' do
   let(:user1) { FactoryBot.create(:user) }
-  # let!(:user2) { FactoryBot.create(:user, first_name: "Tony", last_name: "Stark", email: "ironman123@gmail.com") }
-  let(:deck1) { FactoryBot.create(:deck, user: user1 )}
 
   before do
     login_as(user1)
     visit "/"
+    @deck_title = "Deck Title"
+    @deck_description = "Deck Description"
   end
 
   context "created by users" do
     it "should be visible on a user's decks page" do
       click_link "Manage Decks"
       click_link "Add Deck"
-      fill_in "Title", with: deck1.title
-      fill_in "Description", with: deck1.description
+      fill_in "Title", with: @deck_title
+      fill_in "Description", with: @deck_description
       click_button "Create Deck"
       click_link "All Decks"
 
       expect(page.first("h2").text).to eql("Your Decks")
-      expect(page).to have_content deck1.title
-      expect(page).to have_content deck1.description
+      expect(page).to have_content @deck_title
+      expect(page).to have_content @deck_description
     end
   end 
+
+  context "created by other users" do
+
+    let!(:user2) { FactoryBot.create(:user, first_name: "Tony", last_name: "Stark", email: "ironman456@gmail.com" )}
+    let!(:deck1) { FactoryBot.create(:deck, title: "My Armors", description: "Don't leave home without them", user: user2 )}
+    let!(:deck2) { FactoryBot.create(:deck, title: "Rogues Gallery", description: "baddies", user: user2 )}
+
+    it "should not visible to the current user" do
+      click_link "Manage Decks"
+      click_link "Add Deck"
+      fill_in "Title", with: @deck_title
+      fill_in "Description", with: @deck_description
+      click_button "Create Deck"
+      click_link "All Decks"
+
+      expect(page).not_to have_content deck1.title
+      expect(page).not_to have_content deck2.title
+    end
+  end
 end
