6b20f0fff03ee00df42c74f74ab6a69df1b94e7b
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-14 10:10:18 -0500

    Made statistics page look neater

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -1,2 +1,9 @@
 module AttemptsHelper
+  def percent(n, d)
+    (n.to_f * 100 / d).round
+  end
+  
+  def status(finished, all)
+    finished == all ? "Finished" : "In Progress"
+  end
 end
--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -6,19 +6,19 @@ class Attempt < ApplicationRecord
 
   delegate :title, to: :quiz
 
-  def first_problem
-    problems.where(problem_order: 1).first
+  def correct_problems
+    problems.select(&:correct?)
   end
-
-  def status
-    problems.length == finished_problems.length ? "Finished" : "In Progress"
-  end
-
-  private
 
   def finished_problems
     problems.select(&:answered?)
   end
+
+  def first_problem
+    problems.where(problem_order: 1).first
+  end
+
+  private
 
   def populate_problems
     quiz.questions.shuffle.each.with_index(1) do |q, i|
