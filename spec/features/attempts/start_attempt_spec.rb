4c20db2b3393eda7a6ef4d82680b11a5ff58b857
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-14 08:00:34 -0500

    Users cannot take quizzes if all of its decks are empty

--- spec/features/attempts/start_attempt_spec.rb
+++ spec/features/attempts/start_attempt_spec.rb
@@ -3,7 +3,11 @@ require "rails_helper"
 RSpec.describe "Attempts" do
 
   let!(:user1) { FactoryBot.create(:user) }
+  let!(:user2) { FactoryBot.create(:user) }
+  let!(:user3) { FactoryBot.create(:user) }
   let!(:quiz1) { FactoryBot.create(:quiz_with_decks, user: user1) }
+  let!(:quiz2) { FactoryBot.create(:quiz_with_empty_decks, user: user2) }
+  let!(:quiz3) { FactoryBot.create(:quiz, user: user3) }
 
   describe "can be started by users" do
     it "by clicking the Take test button" do
@@ -14,6 +18,21 @@ RSpec.describe "Attempts" do
       end
 
       # expect page to hvae some stuff
+    end
+  end
+
+  describe "can't be started by users" do
+    it "with quizzes without decks" 
+
+    it "when all decks of a quiz are empty" do
+      login_as(user2)
+      visit quizzes_path
+      Rails.logger.info quizzes_path
+
+      within("div#quiz_#{quiz2.id}") do
+        expect(page).not_to have_button "Take Quiz"
+        expect(page).to have_link "Manage Decks"
+      end
     end
   end
 end
