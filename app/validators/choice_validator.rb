49d674a795e50afe0418217ebad793eaf196f46c
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-14 22:08:13 -0500

    DRYd choice validator

--- app/validators/choice_validator.rb
+++ app/validators/choice_validator.rb
@@ -6,11 +6,8 @@ class ChoiceValidator < ActiveModel::Validator
       if record.choices.size < 2
         record.errors[:base] << "Multiple choice questions should have at least two choices"
       else
-        if true_choices.size > 1
-          record.errors[:base] << "Multiple choice questions should only have one correct choice." 
-        elsif true_choices.size < 1
-          record.errors[:base] << "All Multiple choice options cannot be false"
-        end
+        record.errors[:base] << "Multiple choice questions should only have one correct choice." if true_choices.size > 1 
+        record.errors[:base] << "All Multiple choice options cannot be false" if true_choices.size < 1
       end
     end
   end
