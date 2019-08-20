2bd546efe454537115010740dcbec1dd0f368a9b
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-20 02:38:24 -0500

    Trimmed start attempt spec

--- spec/features/attempts/start_attempt_spec.rb
+++ spec/features/attempts/start_attempt_spec.rb
@@ -4,17 +4,15 @@ RSpec.describe "Attempts" do
 
   let!(:user1) { FactoryBot.create(:user) }
   let!(:user2) { FactoryBot.create(:user) }
-  let!(:user3) { FactoryBot.create(:user) }
-  let!(:quiz1) { FactoryBot.create(:quiz_with_decks, title: "fooquiz", user: user1) }
-  let!(:quiz2) { FactoryBot.create(:quiz_with_empty_decks, user: user2) }
-  let!(:quiz3) { FactoryBot.create(:quiz, user: user3) }
+  let!(:quiz1) { FactoryBot.create(:quiz_with_empty_decks, user: user1) }
+  let!(:quiz2) { FactoryBot.create(:quiz, user: user2) }
 
   describe "can't be started by users" do
     it "with quizzes without decks" do 
-      login_as(user3)
+      login_as(user2)
       visit quizzes_path
 
-      within("div#quiz_#{quiz3.id}") do
+      within("div#quiz_#{quiz2.id}") do
         expect(page).not_to have_button "Take Quiz"
         expect(page).to have_content "This quiz has no decks."
         expect(page).to have_link "Add a deck!"
@@ -22,10 +20,10 @@ RSpec.describe "Attempts" do
    end
 
     it "when all decks of a quiz are empty" do
-      login_as(user2)
+      login_as(user1)
       visit quizzes_path
 
-      within("div#quiz_#{quiz2.id}") do
+      within("div#quiz_#{quiz1.id}") do
         expect(page).not_to have_button "Take Quiz"
         expect(page).to have_link "Manage Decks"
       end
