8ca6d0bf182272f9237c9a6fe9cf7bb6e3ccb465
Jason Bowden <jason.bowden1000@gmail.com>
2019-08-23 18:48:50 -0500

    Attempt stats should not be visible when attempt is in progress

--- spec/features/attempts/continue_attempt_spec.rb
+++ spec/features/attempts/continue_attempt_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe "Incomplete attempts" do
   let!(:user1) { FactoryBot.create(:user) }
   let!(:quiz1) { FactoryBot.create(:quiz_with_decks, user: user1, title: "somequiz") }
 
+  def begin_quiz
+    login_as(user1)
+    visit quizzes_path
+    within("div#quiz_#{quiz1.id}") do
+      click_button "Take Quiz"
+    end
+  end
+
   context "from the stats page" do
     it "can be continued by users" do
-      login_as(user1)
-      visit quizzes_path
-      within("div#quiz_#{quiz1.id}") do
-        click_button "Take Quiz"
-      end
-
+      begin_quiz
       attempt_id = current_path.split("/")[2]
 
       choose("choice_1")
@@ -20,6 +23,17 @@ RSpec.describe "Incomplete attempts" do
       click_link quiz1.title
 
       expect(page.current_path).to eql(attempt_problem_path(attempt_id, 2))
+    end
+
+    it "do not show percent correct" do
+      begin_quiz
+      attempt_id = current_path.split("/")[2]
+      choose("choice_1")
+      click_button "Submit Answer"
+      visit stats_path
+      attempt = find_by_id("attempt_#{attempt_id}")
+      expect(attempt).not_to have_content "100%"
+      expect(attempt).not_to have_content "1/1"
     end
   end
 end
